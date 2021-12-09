7dec880f35459aae3c4a7e1751377f6f31a3da41
Lucas Ramos <87498874+devLucasRamos@users.noreply.github.com>
2021-12-09 20:33:23 -0300

    project Created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VehicleRegistration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
